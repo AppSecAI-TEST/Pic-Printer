35255f0388eda7bf8798885e9ada5d998085a496
nalancer08 <erick.sanchez@thewebchi.mp>
2017-08-04 06:11:44 -0500

    Added

---
+++ TestPrinter/app/src/main/java/com/appbuilders/testprinter/Rester/ReSTCallback.java
@@ -0,0 +1,7 @@
+package com.appbuilders.testprinter.Rester;
+
+public interface ReSTCallback {
+
+    public void onSuccess(ReSTResponse response);
+    public void onError(ReSTResponse response);
+}
